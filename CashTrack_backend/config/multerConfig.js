24d481e3f69b4c1559408ef130d18a75af02fd68
Shefwef <shefayatadib@iut-dhaka.edu>
2025-02-08 16:18:18 +0600

    auth middleware implementation and some other changes

--- CashTrack_backend/config/multerConfig.js
+++ CashTrack_backend/config/multerConfig.js
@@ -19,7 +19,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// Allowed file types
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/png", "application/pdf"];
   if (!allowedTypes.includes(file.mimetype)) {
@@ -28,9 +27,8 @@ const fileFilter = (req, file, cb) => {
   cb(null, true);
 };
 
-// Multer upload instance
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
