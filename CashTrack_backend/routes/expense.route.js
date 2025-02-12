e0393bd7e7c34bc57422460ff85b343913ba7356
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-02-13 05:24:59 +0600

    expense controller and route updated

--- CashTrack_backend/routes/expense.route.js
+++ CashTrack_backend/routes/expense.route.js
@@ -14,7 +14,7 @@ const router = express.Router();
 // CRUD routes with file uploads
 router.post("/", authenticate, upload.single("mediaFile"), createExpense); // Create an expense with media
 router.get("/", authenticate, getExpenses); // Get all expenses with filters
-router.put("/:id", authenticate, upload.single("mediaFile"), updateExpense); // Update an expense with media
+router.patch("/:id", authenticate, updateExpense); // Update an expense with media
 router.delete("/:id", authenticate, deleteExpense); // Delete an expense
 router.get("/media/:filePath", viewMedia); // View media file
 
