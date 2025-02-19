e3c33be826b94f3b70c9deb62fb68669b932b81f
Shefayat E- Shams Adib <shefayatadib@iut-dhaka.edu>
2025-02-20 02:37:56 +0600

    Logical error handled

--- CashTrack_backend/middleware/validation.middleware.js
+++ CashTrack_backend/middleware/validation.middleware.js
@@ -1,7 +1,17 @@
 import { body, validationResult } from "express-validator";
 
 export const validateExpense = [
-  body("date").isISO8601().withMessage("Invalid date format"),
+  body("date")
+    .isISO8601()
+    .withMessage("Invalid date format")
+    .custom((value) => {
+      const inputDate = new Date(value);
+      const today = new Date();
+      if (inputDate > today) {
+        throw new Error("Expense date cannot be in the future");
+      }
+      return true;
+    }),
   body("category").isString().notEmpty().withMessage("Category is required"),
   body("amount").isNumeric().withMessage("Amount must be a number"),
   (req, res, next) => {
